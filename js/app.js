c78d7c7dbe7e831b0f50f8210146ee9b234baac9
Piero <markinpiero005@gmail.com>
2026-01-10 22:26:23 -0500

    funcionalidad de agregar al carrito implementada a la UI

--- js/app.js
+++ js/app.js
@@ -14,18 +14,45 @@ const agregarCurso = (e) => {
     }
 }
 
+const carritoHTML = () => {
+
+    limpiarHTML(contenedorCarrito)
+
+    articulosCarrito.forEach(curso => {
+
+        const { imagen, nombre, precio, cantidad, id } = curso;
+        const row = document.createElement("TR");
+        row.innerHTML = `
+            <td> <img src=${imagen} width="100"> </td>
+            <td> ${nombre} </td>
+            <td> ${precio} </td>
+            <td> ${cantidad} </td>
+            <td> <a href="#" class="borrar-curso" data-id=${id}> X </a>  </td>
+        `
+
+        contenedorCarrito.appendChild(row)
+    })
+}
+
 const leerDatosCurso = (curso) => {
     const infoCurso = {
         imagen: curso.querySelector("img").src,
-        titulo: curso.querySelector("h4").textContent,
+        nombre: curso.querySelector("h4").textContent,
         precio: curso.querySelector(".precio span").textContent,
-        autor: curso.querySelector("p").textContent,
         id: curso.querySelector("a").getAttribute("data-id"),
         cantidad: 1,
     }
-    articulosCarrito = [...articulosCarrito, infoCurso];
+
+    articulosCarrito = [...articulosCarrito, infoCurso]
+
+    carritoHTML()
 }
 
+const limpiarHTML = (referencia) => {
+    while (referencia.firstChild) {
+        referencia.removeChild(referencia.firstChild);
+    }
+}
 
 const cargarEventListener = () => {
     listaCursos.addEventListener("click", agregarCurso);
